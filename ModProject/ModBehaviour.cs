4dc78526332831fba4e1342ad1dd4bf7f0c0e6be
0kk470 <2549366978@qq.com>
2025-11-01 17:17:43 +0800

    update project structure

---
+++ ModProject/ModBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using Duckov.UI;
+using ItemStatsSystem;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Airborne
+{
+
+    public class ModBehaviour : Duckov.Modding.ModBehaviour
+    {
+
+
+        void Awake()
+        {
+
+        }
+        void OnDestroy()
+        {
+
+        }
+    }
+}
